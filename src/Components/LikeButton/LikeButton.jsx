461de24fdde1cdd95ce8e2bf6b49706b9c40cbeb
Cole Massey <cole.massey33@gmail.com>
2022-02-14 03:32:36 -0600

    Like button commented out for now

--- src/Components/LikeButton/LikeButton.jsx
+++ src/Components/LikeButton/LikeButton.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
-import cn from "classnames";
-import { ReactComponent as Hand } from "./hand.svg";
+// import React, { useState } from "react";
+// import cn from "classnames";
+// import { ReactComponent as Hand } from "./hand.svg";
 
-import "./LikeButton.scss";
+// import "./LikeButton.scss";
 
-const LikeButton = () => {
-  const [liked, setLiked] = useState(null);
+// const LikeButton = () => {
+//   const [liked, setLiked] = useState(null);
 
-  return (
-    <button
-      onClick={() => setLiked(!liked)}
-      onAnimationEnd={() => setClicked(false)}
-      className={cn("like-button-wrapper", {
-        liked,
-      })}
-    >
-      <div className="like-button">
-        <Hand />
-        <span>Like</span>
-        <span className={cn("suffix", { liked })}>d</span>
-      </div>
-    </button>
-  );
-};
+//   return (
+//     <button
+//       onClick={() => setLiked(!liked)}
+//       onAnimationEnd={() => setClicked(false)}
+//       className={cn("like-button-wrapper", {
+//         liked,
+//       })}
+//     >
+//       <div className="like-button">
+//         <Hand />
+//         <span>Like</span>
+//         <span className={cn("suffix", { liked })}>d</span>
+//       </div>
+//     </button>
+//   );
+// };
 
-export default LikeButton;
+// export default LikeButton;
