15aef806cdb67b24878bad07a965f6edefdd9f62
Cole Massey <cole.massey33@gmail.com>
2022-02-15 00:31:30 -0600

    Fixed Dislike button. Finished Styling!!!!!

--- src/App.js
+++ src/App.js
@@ -34,20 +34,23 @@ function App() {
   }
 
   return (
-    <div className='container-fluid'>
+    <div class="parent">
+      <div class="div1"> </div>
+      <div class="div2"> </div>
+      <div class="div3">
+      <div className='container-fluid'>
       <div className='row'>
-        <h2 style={{ margin: "1em" }}>
-          Social<small className="text-muted">Feed</small>
-        </h2>
-        <div className='col-xl-6'>
+        </div>
           <div className="border-box">
             <CreatePost addNewPostProperty={addNewPost} />
           </div>
           <div className="border-box">
             <DisplayPosts parentPosts={posts} onLikeClick={onLikeClick} onDislikeClick={onDislikeClick} />
           </div>
-        </div>
+    </div>
       </div>
+      <div class="div4"> </div>
+      <div class="div5"> </div>
     </div>
   );
 }
--- src/Components/DisplayPosts/DisplayPosts.jsx
+++ src/Components/DisplayPosts/DisplayPosts.jsx
@@ -4,16 +4,18 @@ import './DisplayPosts.css'
 
 const DisplayPosts = (props) => {
     return ( 
-        <table>
+        <table className="borderLine">
           {props.parentPosts.map((post, index) => {
             return (
-                <tbody className='border-box'>
-                    <tr>
+                <tbody>
+                    <tr style={{'marginTop': '1em'}}>
                         <th>{post.name}</th>
                     </tr>
-                    <tr>
+                    <tr style={{'marginTop': '1em'}}>
                         <td>{post.post}</td>
-                        <td><LikeButton onLikeClick={props.onLikeClick} onDislikeClick={props.onDislikeClick} index={index} post={post} /></td>
+                    </tr>
+                    <tr style={{'marginTop': '1em'}}>
+                    <td><LikeButton onLikeClick={props.onLikeClick} onDislikeClick={props.onDislikeClick} index={index} post={post}  /></td>
                     </tr>
                 </tbody>
             );
